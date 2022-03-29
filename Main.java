70c401e3d8c9075265f012fcabcabf5ba1aeb246
Doczos <100698947+Doczos@users.noreply.github.com>
2022-03-29 13:29:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main{
+  public static void main(String []args){
+    int i=0;
+    while(i < 10){
+      i++;
+      for(int y=0; y<i; y++)
+        System.out.print("*");
+      System.out.println();
+      
+    }
+  }
+}
